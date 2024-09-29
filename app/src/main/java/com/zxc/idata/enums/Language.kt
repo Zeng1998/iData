62a8d971a17c0fcc51213fd5eb11d53fb9911b96
Zeng1998 <1129142694@qq.com>
2024-09-29 21:07:15 +0800

    feat: support follow-system language

--- app/src/main/java/com/zxc/idata/enums/Language.kt
+++ app/src/main/java/com/zxc/idata/enums/Language.kt
@@ -4,6 +4,7 @@ import com.zxc.idata.R
 import java.util.Locale
 
 enum class Language(val i18nId: Int, val locale: Locale) {
+    FOLLOW_SYSTEM(R.string.follow_system, Locale("follow")),
     EN_US(R.string.en_US, Locale.US),
     ZH_CN(R.string.zh_CN, Locale.CHINA),
 }
--- app/src/main/java/com/zxc/idata/utils/ResouceUtils.kt
+++ app/src/main/java/com/zxc/idata/utils/ResouceUtils.kt
@@ -2,13 +2,15 @@ package com.zxc.idata.utils
 
 import android.content.Context
 import android.content.res.Configuration
+import android.util.Log
 import com.zxc.idata.enums.Language
+import java.util.Locale
 
 
 class StringResourceUtils(private val context: Context, private val language: Language) {
     fun getString(id: Int): String {
         val configuration = Configuration(context.resources.configuration)
-        configuration.setLocale(language.locale)
+        configuration.setLocale(if (language.locale.language == "follow") Locale.getDefault() else language.locale)
         val localizedContext = context.createConfigurationContext(configuration)
         return localizedContext.resources.getString(id)
     }
